81b5fc6fbb52697bec83dc4bc8517ddfed606905
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2023-12-19 13:39:39 +0900

    Update folder directory
    Update folder directory which are data structure, data structure2, tree, math and greedy

--- _daily_problem/230524/01_2230.py
+++ _daily_problem/230524/01_2230.py
@@ -1,1 +1,22 @@
 # 수 고르기
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N, M = map(int, input().split())
+n_list = []
+
+
+for _ in range(N):
+    n_list.append(int(input()))
+n_list.sort()
+dif = n_list[-1] - n_list[0] + 1
+for i in range(N-1):
+    for j in range(N-1, i, -1):
+        now_dif = n_list[j] - n_list[i]
+        if now_dif < dif and now_dif >= M:
+            dif = now_dif
+        if now_dif < M:
+            break
+print(dif)
--- _daily_problem/230524/03_1976.py
+++ _daily_problem/230524/03_1976.py
@@ -1,1 +1,40 @@
 # 여행 가자
+import sys
+from collections import deque
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N = int(input())
+M = int(input())
+graph = {}
+for i in range(1, N+1):
+    graph[i] = []
+
+def BFS(root, depart):
+    visited[root] = True
+    queue = deque([root])
+    while queue:
+        now_node = queue.popleft()
+        if now_node == depart:
+            return True
+        for next_node in graph[now_node]:
+            if next_node != True:
+                queue.append(next_node)
+                visited[next_node] = True
+    return False
+
+for i in range(N):
+    now_list = list(map(int, input().split()))
+    for j in range(N):
+        if now_list[j] == 1:
+            graph[i+1].append(j+1)
+
+plan_path = list(map(int, input().split()))
+for i in range(M-1):
+    visited = [False for _ in range(N+1)]
+    if BFS(plan_path[i], plan_path[i+1]) == False:
+        print("NO")
+        break
+else:
+    print("YES")

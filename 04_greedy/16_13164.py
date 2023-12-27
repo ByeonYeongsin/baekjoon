6fd2353722fc077fce9b6a234486caaf4dfc45e5
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2023-12-27 17:19:56 +0900

    Add problems
    Add problems in greedy section

--- 04_greedy/16_13164.py
+++ 04_greedy/16_13164.py
@@ -1,2 +1,24 @@
 # 13164
 # 행복유치원
+
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N,K = map(int, input().split())
+h = list(map(int, input().split()))
+ 
+arr = []
+ 
+for i in range(N-1):
+    a = h[i+1] - h[i]
+    arr.append(a)
+ 
+arr.sort()
+cost = 0
+ 
+for i in range(N-K):
+    cost += arr[i]
+ 
+print(cost)
